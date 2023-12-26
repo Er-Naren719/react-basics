91e6ef55395dbd4ee10c4d9c042888fda1add75e
Er-Naren719 <patilnarendra3@gmail.com>
2023-12-26 17:36:36 +0530

    Feat: Rendering list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,21 @@
-import { Fragment } from "react"
-
 function ListGroup() {
+  const cities = [
+    'Mumbai',
+    'Delhi',
+    'Banglore',
+    'Hydrabad',
+    'Chennai',
+    'Pune'
+  ];
   return (
-    <Fragment>
+    <>
       <h2>List Group Heading</h2>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {cities.map((city, $index) => (
+          <li key={$index} className="list-group-item">{city}</li>
+        ))}
       </ul>
-    </Fragment>
+    </>
   )
 }
 
